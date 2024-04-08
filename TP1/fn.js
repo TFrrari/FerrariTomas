455d1b560793af54edf3aaaec343b9bcdc025199
TFrrari <tomyferrari66@gmail.com>
2024-04-08 13:03:13 -0300

    actualizacion tp1

---
+++ TP1/fn.js
@@ -0,0 +1,16 @@
+var frutas = ['pera','manzana','frutilla','naranja','kiwi'];
+
+
+for (const fruta of frutas){
+    if (fruta == "manzana"){
+        console.log(fruta);
+    }
+}
+//console.log(frutas[1]);
+
+//function frutas()
+//    for(i=0; i<frutas.length; i++)
+//{
+ //   frutas.forEach(fruta => {console.log(fruta);})
+//}
+//console.log(array)
