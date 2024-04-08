d22a75e6008a91f370e4d0c446a941e053c757d2
TFrrari <tomyferrari66@gmail.com>
2024-04-08 13:07:52 -0300

    arreglos del tp1

---
+++ FuncionesJavaScript/Fn.js
@@ -0,0 +1,9 @@
+
+function saludar(tomas) {
+    alert('Hola '+ tomas);
+  }
+  const aux=50
+
+  function comparacion(num){
+    if (num>aux)alert('es mayor')
+  }
---
+++ FuncionesJavaScript/funciones.js
@@ -0,0 +1,18 @@
+
+function saludar(tomas) {
+    alert('alerta predeterminada');
+  }
+  function comparacion(num) {
+    if (num > 120) {
+        alert("Es mayor");
+    } else if (num < 120) {
+        alert("Es menor");
+    } else {
+        alert("Es igual");
+    }
+}
+
+// Prueba de la función con un valor específico
+let num = 10;
+comparacion(num);
+
