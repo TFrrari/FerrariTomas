7dcd6ca079009c700f115b5df282a71c601c54d3
TFrrari <tomyferrari66@gmail.com>
2024-04-09 12:21:51 -0300

    api RyM

--- TP1/fn.js
+++ TP1/fn.js
@@ -1,4 +1,4 @@
-var frutas = ['pera','manzana','frutilla','naranja','kiwi'];
+/*var frutas = ['pera','manzana','frutilla','naranja','kiwi'];
 
 
 for (const fruta of frutas){
@@ -6,11 +6,34 @@ for (const fruta of frutas){
         console.log(fruta);
     }
 }
-//console.log(frutas[1]);
-
 //function frutas()
 //    for(i=0; i<frutas.length; i++)
 //{
  //   frutas.forEach(fruta => {console.log(fruta);})
 //}
 //console.log(array)
+//--
+//--
+//API Rick y morty*/
+document.addEventListener("DOMContentLoaded", () => {
+    const characterList = document.getElementById("character-list");
+
+    fetch("https://rickandmortyapi.com/api/character")
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(character => {
+                const listItem = document.createElement("li");
+                listItem.innerHTML = `
+                    <div class="character-details">
+                        <h2>${character.name}</h2>
+                        <p>Estado: ${character.status}</p>
+                    </div>
+                `;
+                listItem.className = "character-item";
+                characterList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+        });
+});
