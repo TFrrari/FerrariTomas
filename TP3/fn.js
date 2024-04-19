b202713e91af30cedc4bd533fd956e8b1b2d99ee
lapeire gabriel <lapeiregabriel@gmail.com>
2024-04-19 19:09:50 -0300

    2.0

--- TP3/fn.js
+++ TP3/fn.js
@@ -1,8 +1,10 @@
 var map = L.map('mapid').setView([0, 0], 3); 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}
+    ).addTo(map);
 
 
 function buscarUbicacion() {
