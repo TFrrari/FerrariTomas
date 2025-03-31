a1ce45b171da2a7e3fdb076a9efe0b1ce26e8cc6
TFrrari <151749310+TFrrari@users.noreply.github.com>
2025-03-31 20:17:41 -0300

    tarea edades y colores

--- JS/fn.js
+++ JS/fn.js
@@ -1,38 +1,47 @@
-
-var persona1 = {
-    nombre:"Tomas",
+var persona1= {
+    nombre:"Tomas" ,
     apellido:"ferrari",
     edad:23,
     dni:43828097,
-    colores:["rojo","negro","blanco"]
+    colores:["rojo","amarillo","blanco"] }
+   
+ var persona2= {
+    nombre:"mateo" ,
+    apellido:"ferrari",
+    edad:19,
+    dni:44555668,
+    colores:["negro","azul","blanco"] }
+    
+function edadycolor(persona1,persona2) 
+{
+    if (persona1.edad > persona2.edad) 
+    {
+        console.log(persona1.nombre,persona1.apellido,"es mayor que",persona2.nombre,persona2.apellido)
+        for(let i=0; i<persona1.colores.length;i++)
+            {
+                if (persona1.colores[i] === "azul") 
+                {
+                    console.log("A ",persona1.nombre,"le gusta el color azul")
+                } else {
+                    console.log("A ",persona1.nombre,"NO le gusta el color azul")
+                }
+            } 
     }
-var persona2 = {
-        nombre:"Mateo",
-        apellido:"ferrari",
-        edad:18,
-        dni:43828097,
-        colores:["amarillo","azul","negro"]
-        }
-            function edades (persona2, persona1){
-                var mayor
-                if (persona1.edad>persona2.edad) 
+        else 
+    {
+        console.log(persona2.nombre,persona2.apellido,"es mayor que",persona1.nombre,persona1.apellido) 
+        for(let i=0; i<persona2.colores.length;i++)
+            {
+                if (persona2.colores[i] === "azul") 
                 {
-                console.log(persona1.nombre," es mayor a ",persona2.nombre)
-                if (persona1.colores="azul")
-                (console.log("le gusta el color azul"))
-                else
-                (console.log("no le gusta el color azul"))
+                    console.log("A ",persona2.nombre,"le gusta el color azul")
+                } else {
+                    console.log("A ",persona2.nombre,"NO le gusta el color azul")
                 }
-                else  
-                {
-                console.log(persona2.nombre," es mayor a ",persona1.nombre)
-                if (persona1.colores="azul")
-                (console.log("le gusta el color azul"))
-                else
-                (console.log("no le gusta el color azul"))
-                }
-                return edades;
-             
-            }
-edades(persona1,persona2)
-            
+            }  
+    }
+
+    
+    
+}
+edadycolor(persona1,persona2)
